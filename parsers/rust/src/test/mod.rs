e28a6187cdeb1a72750d005606cb35ccecd42e6b
Chris Joel <0xcda7a@gmail.com>
2022-08-09 11:15:16 -0700

    Remove prototype crate

--- parsers/rust/src/test/mod.rs
+++
@@ -1,2 +0,0 @@
-mod parse;
-mod sequence;
--- parsers/rust/src/util.rs
+++
@@ -1,8 +0,0 @@
-use tendril::{SubtendrilError, Tendril};
-
-pub fn cut<T>(tendril: &Tendril<T>, at: usize) -> Result<Tendril<T>, SubtendrilError>
-where
-    T: tendril::Format,
-{
-    tendril.try_subtendril(at as u32, tendril.len32() - at as u32)
-}
